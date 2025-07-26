3e4c170df6b115fbab153c4ef10971052e15a315
amy <a9055381@gmail.com>
2025-07-26 16:25:11 +0800

    修改日期區間

--- 1.py
+++ 1.py
@@ -1,14 +1,15 @@
 # coding: utf-8
 import requests
 import pandas as pd
+import datetime as dt
 import json
 url = "https://api.cnyes.com/media/api/v1/newslist/category/headline" #新聞連結
 payload = {
     "page":2,
     "limit":30,
     "isCategoryHeadline":1,
-    "startAt":1752644653,
-    "endAt":1753508653
+    'startAt': int((dt.datetime.today()- dt.timedelta(days=11)).timestamp()),
+    'endAt': int(dt.datetime.today().timestamp()),
 } #參數
 res = requests.get(url, params = payload) #連線鉅亨網
 jd = json.loads(res.text) #解析JSON轉成dict
