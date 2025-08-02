050c13d9590ef37f0c209b8fd38a096c03be969e
a905538 <a905538@gmail.com>
2025-08-02 13:45:32 +0800

    test

--- 1.py
+++ 1.py
@@ -18,4 +18,4 @@ df = df[['newsId', 'title', 'summary']]#取出特定欄位
 df['link'] = df['newsId'].apply(lambda  x: 'https://m.cnyes.com/news/id/' + str(x))#建立連結
 df.to_csv('news.csv', encoding = 'utf-8-sig',index =False)
 df.to_excel('news.xlsx', index=False)
-print(df)
+print(df)#123
